68faeb44575e69f721eb71e343a328d9a6c85399
Juanjo2311 <168675820+Juanjo2311@users.noreply.github.com>
2025-03-08 09:51:40 -0500

    HOLAA hola hola

---
+++ main (2).cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+#define forn(i,n) for(int i = 0; i < n; i++)
+
+using namespace std;
+int main()
+{
+	int n,m; cin>>n>>m;
+	map<int,int> mp;
+	while(n-->0){
+	    mp[n];
+	}
+	return 0;
+}
